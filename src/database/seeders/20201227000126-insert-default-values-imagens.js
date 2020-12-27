1f6bcb93997f8c421d808a96c8d15b9743b949e6
CMTEIsrael <israelleao1@hotmail.com>
2020-12-26 21:14:11 -0300

    colocando seeds

--- src/database/seeders/20201227000126-insert-default-values-imagens.js
+++ src/database/seeders/20201227000126-insert-default-values-imagens.js
@@ -3,6 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('imagens', [{
+      chave_condominio: 'default',
       url_image: 'default', 
     }], {});
   },
